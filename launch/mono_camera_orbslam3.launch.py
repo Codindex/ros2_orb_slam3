203b6ed1cba8694a1a9069bbe4f5be0e445d563e
Codindex <61005232+Codindex@users.noreply.github.com>
2026-01-31 18:13:21 +0100

    Add launch file (first version)
    First version for the launch file.
    
    Launch the 2 nodes, with some parameters.
    - namespace
    - name (for each node)
    - ros_parameters file: contains the "orb_slam3_config" parameter, used in the cpp node
    
    I had to add config and launch directory to CMakeLists.txt
    
    Launch with "ros2 launch ros2_orb_slam3 mono_camera_orbslam3.launch.py"

---
+++ launch/mono_camera_orbslam3.launch.py
@@ -0,0 +1,73 @@
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument, ExecuteProcess, TimerAction
+from launch.conditions import IfCondition
+from launch.substitutions import LaunchConfiguration, PythonExpression
+from launch_ros.actions import Node
+
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import PathJoinSubstitution, TextSubstitution
+from launch_ros.substitutions import FindPackageShare
+
+
+def generate_launch_description():
+    orbslam_namespace = LaunchConfiguration('orbslam_namespace')
+    orbslam_node_name = LaunchConfiguration('orbslam_node_name')
+    driver_node_name = LaunchConfiguration('driver_node_name')
+    ros_parameters_file = LaunchConfiguration('ros_parameters_file')
+
+    orbslam_namespace_launch_arg = DeclareLaunchArgument(
+        'orbslam_namespace',
+        default_value='mono_py_driver'
+    )
+    orbslam_node_name_launch_arg = DeclareLaunchArgument(
+        'orbslam_node_name',
+        default_value='driver_test'
+    )
+    driver_node_name_launch_arg = DeclareLaunchArgument(
+        'driver_node_name',
+        default_value='py_driver'
+    )
+    ros_parameters_file_launch_arg = DeclareLaunchArgument(
+        'ros_parameters_file',
+        default_value='rosbag2_2024_09_19.yaml'
+    )
+
+    python_driver_node = Node(
+        package='ros2_orb_slam3',
+        executable='zed_left_driver_node.py',
+        namespace=orbslam_namespace,
+        name=driver_node_name,
+        parameters=[
+            PathJoinSubstitution([
+                FindPackageShare('ros2_orb_slam3'),
+                'config',
+                ros_parameters_file
+            ])
+        ]
+    )
+
+    orbslam3_node = Node(
+        package='ros2_orb_slam3',
+        executable='mono_camera_node_cpp',
+        namespace=orbslam_namespace,
+        name=orbslam_node_name,
+        output='screen',
+        emulate_tty=True,
+        parameters=[
+            PathJoinSubstitution([
+                FindPackageShare('ros2_orb_slam3'),
+                'config',
+                ros_parameters_file
+            ])
+        ]
+    )
+
+    return LaunchDescription([
+        orbslam_namespace_launch_arg,
+        orbslam_node_name_launch_arg,
+        driver_node_name_launch_arg,
+        ros_parameters_file_launch_arg,
+        python_driver_node,
+        orbslam3_node,
+    ])
