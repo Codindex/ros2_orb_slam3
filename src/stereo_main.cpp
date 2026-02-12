e8b01c8e12af93747e75112b7b30e3b2cb90aa1e
Codindex <61005232+Codindex@users.noreply.github.com>
2026-02-12 10:17:23 +0100

    Monocular files copy
    Change names to stereo, and modify header guard name.

---
+++ src/stereo_main.cpp
@@ -0,0 +1,20 @@
+//* Import all necessary modules
+#include "ros2_orb_slam3/monocular.hpp" //* equivalent to orbslam3_ros/include/common.h
+
+//* main
+int main(int argc, char **argv){
+    rclcpp::init(argc, argv); // Always the first line, initialize this node
+    
+    //* Declare a node object
+    auto node = std::make_shared<MonocularNode>(); 
+    
+    // rclcpp::Rate rate(20); // Set the desired update rate (e.g., 10 Hz)
+
+    rclcpp::spin(node); // Blocking node
+    rclcpp::shutdown();
+    return 0;
+}
+
+// ------------------------------------------------------------ EOF ---------------------------------------------
+
+
